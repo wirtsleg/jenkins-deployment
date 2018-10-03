790065122b8a65252722c21a71a0cd10a89070b7
aleksandr <shuto4ka1@gmail.com>
2018-10-03 22:47:37 +0300

    Removed pushing lts.

--- deploy-jenkins-stack.sh
+++ deploy-jenkins-stack.sh
@@ -3,12 +3,10 @@
 DATE=$(date -I)
 
 docker build . --no-cache -t kozhenkov/jenkins-docker:$DATE
-docker tag kozhenkov/jenkins-docker:$DATE kozhenkov/jenkins-docker:lts 
 docker tag kozhenkov/jenkins-docker:$DATE kozhenkov/jenkins-docker:latest
 
 docker push kozhenkov/jenkins-docker:$DATE
 docker push kozhenkov/jenkins-docker:latest
-docker push kozhenkov/jenkins-docker:lts
 
 docker stack rm jenkins
 sleep 15
